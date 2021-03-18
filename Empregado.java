72ffc2c3bee3c09e342dfb360623ee3b85f5aee3
William Ferrari <williamantinarelli.0488@aluno.saojudas.br>
2021-03-18 20:47:20 -0300

    Modificação

--- Empregado.java
+++ Empregado.java
@@ -41,3 +41,22 @@ this.tipo = tipo;
 public int tipo (){
 return this.tipo;
 }
+//adicionar à classe Empregado.java
+public void setSalario (double salario){
+this.salario = salario;
+}
+public double getSalario (){
+return this.salario;
+}
+public void setComissao (double comissao){
+this.comissao = comissao;
+}
+public double getComissao (){
+return this.comissao;
+}
+public void setBonus (double bonus){
+this.bonus = bonus;
+}
+public double getBonus (){
+return this.bonus;
+}
